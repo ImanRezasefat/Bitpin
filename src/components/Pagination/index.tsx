d49b9286f1e2083fbde38a9c412545107b6d969b
Iman rb <iman.rezasefat@gmail.com>
2024-06-01 13:22:21 +0330

    pagination added

---
+++ src/components/Pagination/index.tsx
@@ -0,0 +1,46 @@
+// src/Pagination.js
+import React, { useState } from "react";
+
+export const PaginateComponent = ({
+  totalItems,
+  itemsPerPage,
+  returnCurrentPage,
+}) => {
+  console.log("total items", totalItems);
+  const [currentPage, setCurrentPage] = useState(1);
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
+
+  const handleNext = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage((prevPage) => prevPage + 1);
+      returnCurrentPage(currentPage + 1);
+    }
+  };
+
+  const handlePrev = () => {
+    if (currentPage > 1) {
+      setCurrentPage((prevPage) => prevPage - 1);
+      returnCurrentPage(currentPage - 1);
+    }
+  };
+
+  return (
+    <nav className="flex justify-center gap-2">
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={handlePrev}
+        disabled={currentPage === 1}>
+        Prev
+      </button>
+      <span className="text-gray-800">
+        Page {currentPage} of {totalPages}
+      </span>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={handleNext}
+        disabled={currentPage === totalPages}>
+        Next
+      </button>
+    </nav>
+  );
+};
--- src/containers/Markets/index.tsx
+++ src/containers/Markets/index.tsx
@@ -3,27 +3,42 @@ import { Card } from "@/components/Card";
 import { get } from "@/network";
 import React, { useEffect, useState } from "react";
 import { Tab, TabGroup, TabList, TabPanel, TabPanels } from "@headlessui/react";
+import { PaginateComponent } from "@/components/Pagination";
 
 const Markets = () => {
   const [tabs, setTabs] = useState([]);
+  const [allMarkets, setAllMarkets] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
   const getMarkets = async () => {
     const marketsRes = await get("/mkt/markets/");
-
+    setAllMarkets(marketsRes.results);
+  };
+  //set pagination
+  useEffect(() => {
+    console.log("i set markets", allMarkets);
+    const startIndex = (currentPage - 1) * 10;
+    const itemsPerPage = 10;
     setTabs([
       {
         name: "IRT",
-        markets: marketsRes.results.filter(
+        markets: allMarkets
+          .filter((market) => market.currency2.code === "IRT")
+          .slice(startIndex, startIndex + itemsPerPage),
+        totalItems: allMarkets.filter(
           (market) => market.currency2.code === "IRT"
-        ),
+        ).length,
       },
       {
         name: "USDT",
-        markets: marketsRes.results.filter(
-          (market) => market.currency2.code === "USDT"
-        ),
+        markets: allMarkets
+          .filter((market) => market.currency2.code === "USDT")
+          .slice(startIndex, startIndex + itemsPerPage),
+        totalItems: allMarkets.filter(
+          (market) => market.currency2.code === "IRT"
+        ).length,
       },
     ]);
-  };
+  }, [currentPage, allMarkets]);
   useEffect(() => {
     getMarkets();
   }, []);
@@ -32,7 +47,7 @@ const Markets = () => {
       <div className="flex h-screen w-full justify-center pt-24 px-4">
         <div className="w-full max-w-md">
           <TabGroup>
-            <TabList className="flex gap-4">
+            <TabList className="flex gap-4" onClick={() => setCurrentPage(1)}>
               {tabs.map(({ name }) => (
                 <Tab
                   key={name}
@@ -42,7 +57,7 @@ const Markets = () => {
               ))}
             </TabList>
             <TabPanels className="mt-3">
-              {tabs.map(({ name, markets }) => (
+              {tabs.map(({ name, markets, totalItems }) => (
                 <TabPanel key={name} className="rounded-xl bg-white/5 p-3">
                   <ul>
                     {markets.map((market) => (
@@ -50,6 +65,12 @@ const Markets = () => {
                         <Card market={market} />
                       </div>
                     ))}
+
+                    <PaginateComponent
+                      totalItems={totalItems}
+                      itemsPerPage={10}
+                      returnCurrentPage={(page) => setCurrentPage(page)}
+                    />
                   </ul>
                 </TabPanel>
               ))}
